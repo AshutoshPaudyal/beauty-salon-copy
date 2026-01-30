58d0fdcd948023d60a58a07571bc707b8edf2810
Ashutosh Paudyal <ashutoshpaudyal1@gmail.com>
2026-01-30 21:21:52 +0545

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-  base: '/beauty-salon-copy/', // Replace with YOUR repo name
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve("./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 })
