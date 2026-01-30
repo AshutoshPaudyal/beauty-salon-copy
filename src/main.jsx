0187b667a746b380b79d9f9742e9cef745fd565e
Ashutosh Paudyal <ashutoshpaudyal1@gmail.com>
2026-01-31 01:56:49 +0545

    fix: css issue for the vercel delpoyment

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+console.log('Base font-size:', window.getComputedStyle(document.documentElement).fontSize);
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
-            <App/>
+            <App />
         </BrowserRouter>
     </StrictMode>,
 )
